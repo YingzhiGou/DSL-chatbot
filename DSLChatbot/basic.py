828a9b6e2f4eacae0d7959659844addbf2610013
Yingzhi Gou <29586247+YingzhiGou@users.noreply.github.com>
2018-06-13 09:15:53 +1000

    add profanity filter

--- DSLChatbot/basic.py
+++ DSLChatbot/basic.py
@@ -1,10 +1,19 @@
 import logging
 
+from DSLChatbot.utility.profanityfilter import ProfanitiesFilter
+
 
 class BasicChatBot(object):
-    def __init__(self, name="bot"):
+    def __init__(self, name="bot", language_filter=True):
         self._my_name = name
         self._logger = logging.getLogger(self.__class__.__name__)
+
+        if language_filter:
+            self._logger.info("Profanities Filter Enabled")
+            self._profanities_filter = ProfanitiesFilter()
+        else:
+            self._logger.info("Profanities Filter Disabled")
+            self._profanities_filter = None
 
     def __get_name(self):
         return self._my_name
@@ -16,5 +25,7 @@ class BasicChatBot(object):
 
     def reply(self, input, frm=None):
         answer = self._get_answer(input)
+        if self._profanities_filter:
+            answer = self._profanities_filter.clean(answer)
         self._logger.info("[{}] {}    [{}] {}".format(frm if frm is not None else "USER", input, self._my_name, answer))
         return answer
