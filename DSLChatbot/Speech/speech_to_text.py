b2b8cdb3d4c38b497a199b09a1cd2b15452126c6
Yingzhi Gou <29586247+YingzhiGou@users.noreply.github.com>
2018-08-07 11:18:01 +1000

    updated speech to text example

--- DSLChatbot/Speech/speech_to_text.py
+++ DSLChatbot/Speech/speech_to_text.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python3
 
 # NOTE: this example requires PyAudio because it uses the Microphone class
+import os
 
 import speech_recognition as sr
 
@@ -10,13 +11,14 @@ r = sr.Recognizer()
 # r.pause_threshold =
 with sr.Microphone() as source:
     print("Say something!")
-    audio = r.listen(source, timeout=5, phrase_time_limit=10)
+    r.adjust_for_ambient_noise(source)
+    audio = r.listen(source, timeout=5, phrase_time_limit=15)
 
-r.adjust_for_ambient_noise(audio)
+print("Sample Rate:", audio.sample_rate, "Sample Width:", audio.sample_width)
 
 # recognize speech using Sphinx
 try:
-    print("Sphinx thinks you said " + r.recognize_sphinx(audio))
+    print("Sphinx thinks you said:" + r.recognize_sphinx(audio))
 except sr.UnknownValueError:
     print("Sphinx could not understand audio")
 except sr.RequestError as e:
@@ -27,20 +29,25 @@ try:
     # for testing purposes, we're just using the default API key
     # to use another API key, use `r.recognize_google(audio, key="GOOGLE_SPEECH_RECOGNITION_API_KEY")`
     # instead of `r.recognize_google(audio)`
-    print("Google Speech Recognition thinks you said " + r.recognize_google(audio))
+    print("Google Speech Recognition thinks you said:" + r.recognize_google(audio))
 except sr.UnknownValueError:
     print("Google Speech Recognition could not understand audio")
 except sr.RequestError as e:
     print("Could not request results from Google Speech Recognition service; {0}".format(e))
 
-# # recognize speech using Google Cloud Speech
+# recognize speech using Google Cloud Speech
 # GOOGLE_CLOUD_SPEECH_CREDENTIALS = r"""INSERT THE CONTENTS OF THE GOOGLE CLOUD SPEECH JSON CREDENTIALS FILE HERE"""
-# try:
-#     print("Google Cloud Speech thinks you said " + r.recognize_google_cloud(audio, credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS))
-# except sr.UnknownValueError:
-#     print("Google Cloud Speech could not understand audio")
-# except sr.RequestError as e:
-#     print("Could not request results from Google Cloud Speech service; {0}".format(e))
+with open(os.path.join(os.path.dirname(__file__), "api_key_google_cloud_speech.json"), 'r') as api_key:
+    GOOGLE_CLOUD_SPEECH_CREDENTIALS = api_key.read()
+    # if __debug__:
+    #     print(GOOGLE_CLOUD_SPEECH_CREDENTIALS)
+try:
+    print("Google Cloud Speech thinks you said " + r.recognize_google_cloud(audio,
+                                                                            credentials_json=GOOGLE_CLOUD_SPEECH_CREDENTIALS))
+except sr.UnknownValueError:
+    print("Google Cloud Speech could not understand audio")
+except sr.RequestError as e:
+    print("Could not request results from Google Cloud Speech service; {0}".format(e))
 #
 # # recognize speech using Wit.ai
 # WIT_AI_KEY = "INSERT WIT.AI API KEY HERE"  # Wit.ai keys are 32-character uppercase alphanumeric strings
